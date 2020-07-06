996cbae3948166dd3548c7c8cd90d0f3e3c58de2
Diljot Singh <diljots99@gmail.com>
2020-07-06 15:55:27 +0530

    age and gender detection

--- Face_Functions.py
+++ Face_Functions.py
@@ -17,3 +17,35 @@ def detect_face(frame):
 def draw_rectangle(image, coords):
     for (x, y, w, h) in coords:
         cv2.rectangle(image, (x , y), (x + w , y + h), (0,0,255),2)
+
+def detect_gender(frame,faces):
+
+    genderProto="detectionModel/gender_deploy.prototxt"
+    genderModel="detectionModel/gender_net.caffemodel"
+    genderList=['Male','Female']
+    MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+    genderNet=cv2.dnn.readNet(genderModel,genderProto)
+    
+    ageProto="detectionModel/age_deploy.prototxt"
+    ageModel="detectionModel/age_net.caffemodel"
+    ageNet=cv2.dnn.readNet(ageModel,ageProto)
+    ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    padding=20
+    for faceBox in faces:
+        face=frame[max(0,faceBox[1]-padding):
+                   min(faceBox[3]+padding,frame.shape[0]-1),max(0,faceBox[0]-padding)
+                   :min(faceBox[2]+padding, frame.shape[1]-1)]
+        try:
+           
+            blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+            genderNet.setInput(blob)
+            genderPreds=genderNet.forward()
+            gender=genderList[genderPreds[0].argmax()]
+            print(f'Gender: {gender}')
+
+            ageNet.setInput(blob)
+            agePreds=ageNet.forward()
+            age=ageList[agePreds[0].argmax()]
+            print(f'Age: {age[1:-1]} years')
+        except :
+            pass
--- face.py
+++ face.py
@@ -2,6 +2,8 @@ import cv2
 
 from tkinter import *
 from Face_Functions import *
+import numpy as np
+from PIL import Image, ImageTk
 
 root = Tk()
 
@@ -9,30 +11,49 @@ vid = cv2.VideoCapture(0)
 
 
 
+def live_Capture():
 
-while(True): 
-      
-    # Capture the video frame 
-    # by frame 
+    while(True): 
+
+
+        # Capture the video frame 
+        # by frame 
+        ret, frame = vid.read() 
+    
+        faces_coord = detect_face(frame)
+        draw_rectangle(frame, faces_coord)
+        if len(faces_coord):
+            detect_gender(frame,faces_coord)
+        print (faces_coord)
+
+        #TODO predict if face exixts 
+
+        if False:
+            #TODO  when face already exists in database show the relatcent atributes
+            pass
+        else:
+            pass
+
+        # Display the resulting frame 
+        cv2.imshow('frame', frame) 
+
+        # the 'q' button is set as the 
+        # quitting button you may use any 
+        # desired button of your choice 
+        if cv2.waitKey(1) & 0xFF == ord('q'): 
+            break
+
+
+    # After the loop release the cap object 
+    vid.release() 
+    # Destroy all the windows 
+    cv2.destroyAllWindows() 
+
+def Add_face():
+        
+    vid = cv2.VideoCapture(0)
+    #cv2.namedWindow("Face", cv2.WINDOW_AUTOSIZE)
     ret, frame = vid.read() 
-  
-    faces_coord = detect_face(frame)
-    draw_rectangle(frame, faces_coord)
-    print (faces_coord)
+    ad = cv2.imshow('frame', frame) 
 
-   
-
-    # Display the resulting frame 
-    cv2.imshow('frame', frame) 
-      
-    # the 'q' button is set as the 
-    # quitting button you may use any 
-    # desired button of your choice 
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
-        break
-
-
-# After the loop release the cap object 
-vid.release() 
-# Destroy all the windows 
-cv2.destroyAllWindows() 
+live_Capture()
